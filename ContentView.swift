d47faaccd7b1f9a26a0b576a66a89592dc43e73a
aditianaaw <adtnaw@gmail.com>
2021-01-06 23:05:42 +0700

    Test Aja

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutomationTest
+//
+//  Created by Admin2 on 25/11/20.
+//  Copyright © 2020 Admin2. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
